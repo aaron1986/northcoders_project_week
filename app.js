38ff1515de6aa8f6d99867c18c04606de056434c
aaron1986 <aaronbrucesmith86@gmail.com>
2024-11-26 17:07:22 +0000

    task10 passing but need to write errors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const {
     postComment,
     patchArticle,
     deleteComment,
+    getUsers,
 } = require('./db/controller/topics.controller')
     
 const app = express()
@@ -21,6 +22,8 @@ app.get("/api/topics", getTopics)
 app.get('/api/articles/:article_id', getArticleById);
 app.get('/api/articles', getArticles);
 app.get("/api/articles/:article_id/comments", getComments)
+
+app.get("/api/users", getUsers)
 
 //POST api and endPoints
 app.post('/api/articles/:article_id/comments', postComment)
