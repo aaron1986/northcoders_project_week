2dc319b34ddcfaf04bff62052dd4e060325c59a1
aaron1986 <aaronbrucesmith86@gmail.com>
2024-11-29 11:04:09 +0000

    task 17 completed

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getUsers } = require("../db/controller/topics.controller");
+const { getUsers, getUsername } = require("../db/controller/topics.controller");
 
 const usersRouter = express.Router();
 
 usersRouter.get("/", getUsers);
+usersRouter.get("/:username", getUsername);
 
 module.exports = usersRouter;
