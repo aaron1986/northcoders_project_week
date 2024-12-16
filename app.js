d3be5e4654aebf42fde0de1eb0da4396871dd764
aaron1986 <aaronbrucesmith86@gmail.com>
2024-12-16 12:50:37 +0000

    cors updated

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const apiRouter = require("./routes/api-router");
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
-const cors = require('cors');
-app.use(cors());
 
 //example for postman
 app.get("/test", async (req, res) => {
